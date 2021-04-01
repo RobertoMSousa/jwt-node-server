2f536d84c2c8aebb4657ef7a64a9bf006c906716
Roberto Sousa <beto.sousa22@gmail.com>
2021-04-01 23:56:51 +0100

    basic styling

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,22 @@
 import express, {Response, Request} from 'express';
-
-
-
-
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded());
 
 
 // Routes
+// login route
 app.post('/login',(_req: Request, res: Response)=>{
   res.status(200).json();
 });
+
+// logout route
 app.post('/logout', (_req: Request, res: Response)=>{
   console.log('🚀 - logout'); // roberto
   res.status(200).json();
 });
+
+// a secret page that can only be accessed when logged in.
 app.get('/secret_page', (_req: Request, res: Response)=>{
   console.log('get secrete page')
   res.status(200).json({secret: 'my protected info'})
